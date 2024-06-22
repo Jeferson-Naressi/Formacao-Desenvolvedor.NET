251046fd40b17186d1dcf6201c5a8469ef5c7c8a
Jeferson <jeferson143@hotmail.com>
2024-06-22 10:59:35 -0300

    Cadastro de Alunos
    Primeiro Projeto em .NET MVC

---
+++ ASP.NET MVC/PrimeiroProjetoMVC/PrimeiroProjetoMVC/Models/Aluno.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimeiroProjetoMVC.Models
+{
+    public class Aluno
+    {
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DataCadastro { get; set; }
+        public bool Ativo { get; set; }
+    }
+}
