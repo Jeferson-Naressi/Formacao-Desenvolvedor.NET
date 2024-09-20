908ad15ae268e2bd1211654695baf681a8387477
Jeferson <93160555+Jeferson-Naressi@users.noreply.github.com>
2024-09-20 07:51:59 -0300

    Update Program.cs

--- Introdução ao Entity Framework Core/SiteDeFilmes/Program.cs
+++ Introdução ao Entity Framework Core/SiteDeFilmes/Program.cs
@@ -39,20 +39,7 @@ public static T Adicionar<T>(T entidade) where T : class
     return entidade;
 }
 
-    // Adicionar relações de elenco
-    var elenco1 = new ElencoFilmes { IdAtor = ator1.Id, IdFilmes = filme1.Id, Papel = "Cobb" };
-    var elenco2 = new ElencoFilmes { IdAtor = ator2.Id, IdFilmes = filme2.Id, Papel = "Miranda" };
-    context.ElencoFilmes.AddRange(elenco1, elenco2);
-    context.SaveChanges();
 
-    // Associar gêneros aos filmes
-    var filmeGenero1 = new FilmesGeneros { IdFilmes = filme1.Id, IdGenero = generoAcao.Id };
-    var filmeGenero2 = new FilmesGeneros { IdFilmes = filme2.Id, IdGenero = generoComedia.Id };
-    context.FilmesGeneros.AddRange(filmeGenero1, filmeGenero2);
-    context.SaveChanges();
-}
-}
-}
 
 // Supondo que a classe Atores já está definida
 var novoAtor = new Atores
