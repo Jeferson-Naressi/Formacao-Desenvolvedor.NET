13285c822e413d8f98056d0bda70b0e7ca5bc65f
Jeferson <93160555+Jeferson-Naressi@users.noreply.github.com>
2024-09-23 11:38:12 -0300

    Update Program.cs

--- Introdução ao Entity Framework Core/SiteDeFilmes/Program.cs
+++ Introdução ao Entity Framework Core/SiteDeFilmes/Program.cs
@@ -1,49 +1,81 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
-
-using (var context = new FilmeContext())
-{
-    // Adicionando Gêneros
-    var acao = new Generos { Genero = "Ação" };
-    var comedia = new Generos { Genero = "Comédia" };
-    context.Generos.AddRange(acao, comedia);
-    context.SaveChanges();
-
-    // Adicionando Atores
-    var ator1 = new Atores { PrimeiroNome = "Jeferson", UltimoNome = "Naressi", Genero = "Masculino" };
-    var ator2 = new Atores { PrimeiroNome = "Jane", UltimoNome = "Lys", Genero = "Feminino" };
-    context.Atores.AddRange(ator1, ator2);
-    context.SaveChanges();
-
-    // Adicionando Filmes
-    var filme1 = new Filmes { Nome = "Aventura Incrível", Ano = 2022, Duracao = 120 };
-    var filme2 = new Filmes { Nome = "Riso Garantido", Ano = 2023, Duracao = 90 };
-    context.Filmes.AddRange(filme1, filme2);
-    context.SaveChanges();
-
-    // Adicionando Elenco
-    context.ElencoFilmes.AddRange(
-        new ElencoFilmes { IdAtor = ator1.Id, IdFilmes = filme1.Id, Papel = "Heroi" },
-        new ElencoFilmes { IdAtor = ator2.Id, IdFilmes = filme2.Id, Papel = "Protagonista" }
-    );
-    context.SaveChanges();
-
-    // Associando Filmes e Gêneros
-    context.FilmesGeneros.AddRange(
-        new FilmesGeneros { IdFilmes = filme1.Id, IdGenero = acao.Id },
-        new FilmesGeneros { IdFilmes = filme2.Id, IdGenero = comedia.Id }
-    );
-    context.SaveChanges();
-}
-private static void ConsultarDados()
+private static void AdicionarAtor()
 {
     using var db = new Data.ApplicationContext();
-    //var consultarPorSintaxe = (from c in db.filmes where c.Id>0 select c).ToList();
-    var ConsultaPorMetodo = db.Filmes.Where(p =>p.Id >0).ToList();
-    foreach(var Filmes in ConsultaPorMetodo)
+
+    var novoAtor = new Atores
     {
-        db.Filmes.Find(Filme.Id) //Find consulta primeiro em memoria caso não encontre procura no bd
-    }
-    
+        PrimeiroNome = "Nome do Ator",
+        UltimoNome = "Sobrenome do Ator",
+        Genero = "Gênero"
+    };
+
+    db.Atores.Add(novoAtor);
+    db.SaveChanges();
+
+    Console.WriteLine("Ator adicionado com sucesso!");
+}
+
+private static void AdicionarFilme()
+{
+    using var db = new Data.ApplicationContext();
+
+    var novoFilme = new Filmes
+    {
+        Nome = "Nome do Filme",
+        Ano = 2023,
+        Duracao = 120 // Duração em minutos
+    };
+
+    db.Filmes.Add(novoFilme);
+    db.SaveChanges();
+
+    Console.WriteLine("Filme adicionado com sucesso!");
+}
+
+private static void AdicionarGenero()
+{
+    using var db = new Data.ApplicationContext();
+
+    var novoGenero = new Generos
+    {
+        Genero = "Nome do Gênero"
+    };
+
+    db.Generos.Add(novoGenero);
+    db.SaveChanges();
+
+    Console.WriteLine("Gênero adicionado com sucesso!");
+}
+
+private static void AdicionarElenco(int idAtor, int idFilme, string papel)
+{
+    using var db = new Data.ApplicationContext();
+
+    var novoElenco = new ElencoFilmes
+    {
+        IdAtor = idAtor,
+        IdFilmes = idFilme,
+        Papel = papel
+    };
+
+    db.ElencoFilmes.Add(novoElenco);
+    db.SaveChanges();
+
+    Console.WriteLine("Elenco adicionado com sucesso!");
+}
+
+private static void AssociarFilmeGenero(int idFilme, int idGenero)
+{
+    using var db = new Data.ApplicationContext();
+
+    var novaAssociacao = new FilmesGeneros
+    {
+        IdFilmes = idFilme,
+        IdGenero = idGenero
+    };
+
+    db.FilmesGeneros.Add(novaAssociacao);
+    db.SaveChanges();
+
+    Console.WriteLine("Filme e gênero associados com sucesso!");
 }
